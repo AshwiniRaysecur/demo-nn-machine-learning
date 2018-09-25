a98cf5bfc006feed42470e37e50cdb06e615fde5
NIck Nagel <nicknagel@NIcks-MBP.home>
2018-09-25 15:27:27 -0400

    canvas stuff getting sorted

--- src/MachineLearningSystem/index.js
+++ src/MachineLearningSystem/index.js
@@ -5,23 +5,85 @@ import catInput from './images/i1-cat.png';
 
 class MachineLearningSystem extends Component {
 
+    constructor(props) {
+        super(props);
+
+        this.drawCB = this.drawCB.bind(this);
+        this.getImageData = this.getImageData.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+
+
+    }
+
     componentDidMount() {
-        const canvas = this.refs.domCanvas1;
+        this.canvas = this.refs.domCanvas1;
 
-        const img    = this.refs.domImage1;
+        // const img    = this.refs.domImage1;
 
-        const ctx = canvas.getContext("2d");
+        this.ctx = this.canvas.getContext("2d");
 
         // ctx.fillStyle = "red";
         // ctx.fillRect(0, 0, 200, 180);
         // ctx.drawImage(catInput, 0, 0);
 
-        var imageObj = new Image();
-        imageObj.src = './images/i1-cat.png'
-        imageObj.onload = function() {
-            ctx.drawImage(imageObj,0,0);
-        }
+        this.imageObj = new Image();
+        this.imageObj.src = './images/i1-cat.png'
+        // this.imageObj.onload = this.drawCB;
 
+    }
+
+    drawCB() {
+        this.ctx.drawImage( this.imageObj,0,0 );
+    }
+
+    getImageData ( imgObject ) {
+        this.ctx.drawImage( this.imageObj,0,0 );
+        return( this.ctx.getImageData(0, 0, 125, 125) );
+    }
+
+    preview() {
+
+        // $('#iterations').text(trial);
+
+        this.ctx.drawImage( this.imageObj,0,0 );
+
+        const  imageData = this.ctx.getImageData(0, 0, 125, 125);
+
+
+
+
+
+        // for (index = 0; index < size; index++)
+        // {
+        //     var px = pixel(original_data, 0, 0);
+        //     px = px.concat(pixel(original_data, -1, -1));
+        //     px = px.concat(pixel(original_data, 0, -1));
+        //     px = px.concat(pixel(original_data, 1, -1));
+        //     px = px.concat(pixel(original_data, -1, 0));
+        //     px = px.concat(pixel(original_data, 1, 0));
+        //     px = px.concat(pixel(original_data, -1, 1));
+        //     px = px.concat(pixel(original_data, 0, 1));
+        //     px = px.concat(pixel(original_data, 1, 1));
+        //     var rgb = perceptron.activate(px);
+        //     imageData.data[index * 4] = (rgb[0] )* 255;
+        //     imageData.data[index * 4 + 1] = (rgb[1] ) * 255;
+        //     imageData.data[index * 4 + 2] = (rgb[2] ) * 255;
+        // }
+
+
+        this.ctx.putImageData(imageData,0,0);
+
+        // if ($location.$$path == '/image-filters')
+        //     setTimeout(iteration, 100);
+    }
+
+
+
+    handleClick(evt) {
+
+        console.log(evt.target);
+
+        this.preview();
 
     }
 
@@ -54,6 +116,12 @@ class MachineLearningSystem extends Component {
                         src="./images/o1-cat-texture.png"
                     />
                 </div>
+
+                <button
+
+                    onClick={ this.handleClick }
+
+                >click me</button>
 
 
             </div>
