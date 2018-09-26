b69b690ee50813ce3157d1e2e54b2df205c1482d
NIck Nagel <nicknagel@NIcks-MacBook-Pro.local>
2018-09-26 13:33:54 -0400

    iterations set up

--- src/MachineLearningSystem/index.js
+++ src/MachineLearningSystem/index.js
@@ -1,26 +1,68 @@
 import React, {Component} from 'react';
 import { NavLink } from 'react-router-dom';
 
-import catInput from './images/i1-cat.png';
+import {Architect} from 'synaptic';
+
+
+
+const image = {
+    ORIGINAL: './images/cat_original.png',
+    INPUT:    './images/i1-cat.png',
+    FILTERED: './images/o1-cat-texture.png'
+};
+
+const ITERATIONS = 50;
 
 class MachineLearningSystem extends Component {
 
     constructor(props) {
         super(props);
 
-        this.imageObj     = new Image();
-        this.imageObj.src = './images/i1-cat.png';
+        this.state = {
+            trial:0
+        };
+
+        // this.imageObj     = new Image();
+        // this.imageObj.src = './images/i1-cat.png';
+
+        // create a new 3 layor perceptron with one input one hidden and one output layor
+        this.perceptron = new Architect.Perceptron(27,8,3);
+
+        this.index      = 0;
+        this.size       = 125 * 125;
+
+        this.px         = null;
+
+        // data
+        this.imageOjbects = {
+            'input': new Image(),
+            'output': new Image(),
+            'original': new Image()
+        };
+
+        this.imageOjbects.input.src    = image.INPUT;
+        this.imageOjbects.output.src   = image.FILTERED;
+        this.imageOjbects.original.src = image.ORIGINAL;
+
+
 
         //this.drawCB = this.drawCB.bind(this);
 
         this.getImageData = this.getImageData.bind(this);
-        this.handleClick = this.handleClick.bind(this);
+        this.handleClick  = this.handleClick.bind(this);
+        this.iteration    = this.iteration.bind(this);
 
     }
 
     componentDidMount() {
         this.canvas = this.refs.domCanvas1;
         this.ctx = this.canvas.getContext("2d");
+
+
+        this.colorData    = this.getImageData( this.imageOjbects.input );
+        this.originalData = this.getImageData( this.imageOjbects.original );
+        this.filterData   = this.getImageData( this.imageOjbects.output );
+
     }
 
     // drawCB() {
@@ -28,54 +70,95 @@ class MachineLearningSystem extends Component {
     // }
 
     getImageData ( imgObject ) {
-        this.ctx.drawImage( this.imageObj,0,0 );
+
+        this.ctx.drawImage( imgObject, 0, 0 );
         return( this.ctx.getImageData(0, 0, 125, 125) );
+
+    }
+
+    pixel (data, ox, oy) {
+        let y = this.index / 125 | 0;
+        let x = this.index % 125;
+
+        if (ox && (x + ox) > 0 && (x + ox) < 125)
+            x += ox;
+        if (oy && (y + oy) > 0 && (y + oy) < 125)
+            y += oy;
+
+        const red = data[((125 * y) + x) * 4];
+        const green = data[((125 * y) + x) * 4 + 1];
+        const blue = data[((125 * y) + x) * 4 + 2];
+
+        return [red / 255, green / 255, blue / 255];
+    }
+
+    iteration() {
+        let t = this.state.trial;
+        this.setState( {trial: ++t} );
+
+
+        for ( this.index = 0; this.index < this.size; this.index+=2 ) {
+
+            this.px = this.pixel( this.colorData, 0, 0 );
+
+            this.px = this.px.concat( this.pixel( this.colorData, -1, -1 ) );
+            this.px = this.px.concat( this.pixel( this.colorData, 0, -1 ) );
+            this.px = this.px.concat( this.pixel( this.colorData, 1, -1 ) );
+            this.px = this.px.concat( this.pixel( this.colorData, -1, 0 ) );
+            this.px = this.px.concat( this.pixel( this.colorData, 1, 0 ) );
+            this.px = this.px.concat( this.pixel( this.colorData, -1, 1 ) );
+            this.px = this.px.concat( this.pixel( this.colorData, 0, 1 ) );
+            this.px = this.px.concat( this.pixel( this.colorData, 1, 1 ) );
+
+            this.perceptron.activate(this.px);
+            this.perceptron.propagate(.12, this.pixel( this.filterData, 0, 0) );
+        }
+
+        this.preview();
+
     }
 
     preview() {
 
         // $('#iterations').text(trial);
-
-        this.ctx.drawImage( this.imageObj,0,0 );
+        // todo RESUME HERE
+        // todo this is scaffolding. here is where you actually show each iteration!!
+        // this.ctx.drawImage( this.imageOjbects.original  , 0, 0 );
 
         const  imageData = this.ctx.getImageData(0, 0, 125, 125);
 
-
-
-
-
-        // for (index = 0; index < size; index++)
-        // {
-        //     var px = pixel(original_data, 0, 0);
-        //     px = px.concat(pixel(original_data, -1, -1));
-        //     px = px.concat(pixel(original_data, 0, -1));
-        //     px = px.concat(pixel(original_data, 1, -1));
-        //     px = px.concat(pixel(original_data, -1, 0));
-        //     px = px.concat(pixel(original_data, 1, 0));
-        //     px = px.concat(pixel(original_data, -1, 1));
-        //     px = px.concat(pixel(original_data, 0, 1));
-        //     px = px.concat(pixel(original_data, 1, 1));
-        //     var rgb = perceptron.activate(px);
-        //     imageData.data[index * 4] = (rgb[0] )* 255;
-        //     imageData.data[index * 4 + 1] = (rgb[1] ) * 255;
-        //     imageData.data[index * 4 + 2] = (rgb[2] ) * 255;
-        // }
+        for ( this.index = 0; this.index < this.size; this.index++) {
+            let px = this.pixel( this.originalData, 0, 0 );
+            px = px.concat( this.pixel( this.originalData, -1, -1 ) );
+            px = px.concat( this.pixel( this.originalData, 0, -1 ) );
+            px = px.concat( this.pixel( this.originalData, 1, -1 ) );
+            px = px.concat( this.pixel( this.originalData, -1, 0 ) );
+            px = px.concat( this.pixel( this.originalData, 1, 0 ) );
+            px = px.concat( this.pixel( this.originalData, -1, 1 ) );
+            px = px.concat( this.pixel( this.originalData, 0, 1 ) );
+            px = px.concat( this.pixel( this.originalData, 1, 1 ) );
+            let rgb = this.perceptron.activate(px);
+            imageData.data[this.index * 4] = (rgb[0] )* 255;
+            imageData.data[this.index * 4 + 1] = (rgb[1] ) * 255;
+            imageData.data[this.index * 4 + 2] = (rgb[2] ) * 255;
+        }
 
 
         this.ctx.putImageData(imageData,0,0);
 
         // if ($location.$$path == '/image-filters')
-        //     setTimeout(iteration, 100);
+        if( this.state.trial < ITERATIONS )
+            setTimeout( this.iteration, 100 );
+
     }
 
 
 
     handleClick(evt) {
-
-        console.log(evt.target);
-
-        this.preview();
-
+        let t = 0;
+        this.setState( {trial: t} );
+        console.log( "calling iteration" );
+        this.iteration();
     }
 
     render() {
@@ -107,6 +190,13 @@ class MachineLearningSystem extends Component {
                         src="./images/o1-cat-texture.png"
                     />
                 </div>
+
+
+                <div className="trials-wrap">
+                    {this.state.trial}
+                </div>
+
+
 
                 <button
 
